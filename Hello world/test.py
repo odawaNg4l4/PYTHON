e08654dbcbf1d2176ca23bbcd3260ad29797141b
odawaNg4l4 <odawangala99@gmail.com>
2024-11-18 13:47:52 +0300

    Hello World

---
+++ Hello world/test.py
@@ -0,0 +1,12 @@
+print("Hello world")
+user_name = input("Enter your name")
+print("Hello", user_name)
+
+name = input("Enter your name")
+print("Hello", name)
+name = input("Enter bestfriends's name")
+print("Hello", name, "bestfriend")
+
+numbers = 4, 5
+total_sum = sum(numbers)
+print("The total sum is:", total_sum )

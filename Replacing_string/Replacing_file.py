19a46b7be41e0ee639034cf1709d4e38970d775e
odawaNg4l4 <odawangala99@gmail.com>
2024-11-19 19:34:34 +0300

    Split String

---
+++ Replacing_string/Replacing_file.py
@@ -0,0 +1,7 @@
+sentence = 'Denyse,Marie,Smith,21,London,UK'
+print('Source string:', sentence)
+print('Split using a comma')
+print(sentence.split(','))
+print('Split using a space')
+print(sentence.split(' '))
+

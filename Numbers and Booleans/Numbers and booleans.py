44aa505a3b1c258bae2f00adbbeed2fd43a45125
odawaNg4l4 <odawangala99@gmail.com>
2024-11-22 10:39:58 +0300

    Numbers and boolean

--- Numbers and Booleans/Numbers and booleans.py
+++ Numbers and Booleans/Numbers and booleans.py
@@ -13,3 +13,16 @@ print(type(float_value))
 float_value = float(string_value)
 print('string value as a float:', float_value)
 print(type(float_value))
+
+# Converting an input string
+exchange_rate = float(input("Please enter the exchange rate to use:"))
+print(exchange_rate)
+
+# Complex numbers
+c1 = 1j
+c2 = 2j
+print('c1:', c1, 'c2:', c2)
+print(type(c1))
+print(c1.real)
+print(c1.imag)
+

28d0ac3dcee9660e60e5536bf2e465ed20bbdfea
odawaNg4l4 <odawangala99@gmail.com>
2024-11-22 10:17:45 +0300

    Numbers and booleans

---
+++ Numbers and Booleans/Numbers and booleans.py
@@ -0,0 +1,15 @@
+age = int(input('Please enter your age'))
+print(type(age))
+print(age)
+
+exchange_rate = 1.86
+print(type(exchange_rate))
+
+int_value = 1
+string_value = '1.5'
+float_value = float(int_value)
+print('int value as a float;', float_value)
+print(type(float_value))
+float_value = float(string_value)
+print('string value as a float:', float_value)
+print(type(float_value))

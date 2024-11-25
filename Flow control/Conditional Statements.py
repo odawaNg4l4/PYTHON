88249bbb02d126f6227b8e30a503c2af7af79997
odawaNg4l4 <odawangala99@gmail.com>
2024-11-25 10:01:37 +0300

    Conditional Statements

--- Flow control/Conditional Statements.py
+++ Flow control/Conditional Statements.py
@@ -7,7 +7,7 @@ if num > 0:
     print((num, 'is squared', num * num))
     print('bye')
 
-# or you can just
+# or you can just else
 num = int(input('Enter a number'))
 if num < 0:
     print(num,'is negative')
@@ -15,3 +15,17 @@ else:
     print(num, 'is positive')
     print((num, 'is squared', num * num))
 print('Bye')
+
+# elif
+savings = float(input('Enter how much you have in savings:'))
+if savings == 0:
+    print("Sorry no savings")
+elif savings < 500:
+    print("A litu bit of money")
+elif savings < 1000:
+    print("Promising amount")
+elif savings < 10000:
+    print("Welcome aboard maam")
+else:
+    print("Thank you!")
+

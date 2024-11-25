5d4cc42ad225e2f3ca9db2fb1a422dab44f8d438
odawaNg4l4 <odawangala99@gmail.com>
2024-11-25 09:40:08 +0300

    Conditional statements

--- Flow control/Conditional Statements.py
+++ Flow control/Conditional Statements.py
@@ -1,3 +1,8 @@
 num = int(input('Enter a number'))
 if num < 0:
-    print('Num is negative')
+    print(num,'is negative')
+num = int(input('Enter another number:'))
+if num > 0:
+    print(num, 'is positive')
+    print((num, 'is squared', num * num))
+    print('bye')

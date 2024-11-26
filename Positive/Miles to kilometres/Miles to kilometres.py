6c7871ffcc738ec47a77c32c6a0127efe7e881d5
odawaNg4l4 <odawangala99@gmail.com>
2024-11-26 17:53:27 +0300

    Miles to kilometres

--- Positive/Miles to kilometres/Miles to kilometres.py
+++ Positive/Miles to kilometres/Miles to kilometres.py
@@ -1,2 +1,5 @@
 distance_kilometres = int(input('Give the distance in kilometres'))
-print("Distance in miles is:",    distance_kilometres * 0.6214 )
+if distance_kilometres >= 0:
+    print("Distance in miles is:",    distance_kilometres * 0.6214 )
+else:
+    print("It is wrong")

c1a7df53f1eb26bf47fcd74f3b6946d69c7467be
odawaNg4l4 <odawangala99@gmail.com>
2024-11-27 13:42:50 +0300

    Iteration

--- Positive/Iteration/Iteration.py
+++ Positive/Iteration/Iteration.py
@@ -1,22 +1,45 @@
-# while
-count = 0
-print('Starting')
-while count < 10:
-    print(count, '', end = '')
-    count += 1
-print()
-print('Done')
+# # # while
+# # count = 0
+# # print('Starting')
+# # while count < 10:
+# #     print(count, '', end = '')
+# #     count += 1
+# # print()
+# # print('Done')
+# #
+# # # for
+# # print('Print out values in a range')
+# # for i in range(0, 10):
+# #     print(i, '', end='')
+# # print()
+# # print('Done')
+# #
+# # # incremented by 2
+# # print('Print out values in a range')
+# # for i in range(0, 10, 2):
+# #     print(i, '', end='')
+# # print()
+# # print('Done')
+# #
+# # # Anonymous loop variable
+# # for _ in range(0, 10):
+# #     print('.', end='')
+# # print()
+#
+# # Break loop statement
+# print('Only print code if all iterations completed')
+# num = int(input('Enter a number to check for:'))
+# for i in range(0, 6):
+#     if i ==num:
+#         break
+#     print(i, '', end='')
+# print('Done')
 
-# for
-print('Print out values in a range')
+# Continue loop
 for i in range(0, 10):
     print(i, '', end='')
-print()
-print('Done')
-
-# incremented by 2
-print('Print out values in a range')
-for i in range(0, 10, 2):
-    print(i, '', end='')
-print()
+    if i % 2  == 1:
+        continue
+    print('Hey its an even number')
+    print('We love even numbers')
 print('Done')

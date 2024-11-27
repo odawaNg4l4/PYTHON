7a8375521dfb9934209b8264b7b033be09a1e423
odawaNg4l4 <odawangala99@gmail.com>
2024-11-27 12:02:57 +0300

    Iteration

--- Positive/Iteration/Iteration.py
+++ Positive/Iteration/Iteration.py
@@ -1,7 +1,15 @@
+# while
 count = 0
 print('Starting')
 while count < 10:
     print(count, '', end = '')
     count += 1
+print()
+print('Done')
+
+# for
+print('Print out values in a range')
+for i in range(0, 10):
+    print(i, '', end='')
 print()
 print('Done')

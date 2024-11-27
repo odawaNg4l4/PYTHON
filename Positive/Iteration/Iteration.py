016acb5ca1b57cbe8bd0fa76219abe4d6c287b87
odawaNg4l4 <odawangala99@gmail.com>
2024-11-27 12:05:51 +0300

    Iteration

--- Positive/Iteration/Iteration.py
+++ Positive/Iteration/Iteration.py
@@ -13,3 +13,10 @@ for i in range(0, 10):
     print(i, '', end='')
 print()
 print('Done')
+
+# incremented by 2
+print('Print out values in a range')
+for i in range(0, 10, 2):
+    print(i, '', end='')
+print()
+print('Done')

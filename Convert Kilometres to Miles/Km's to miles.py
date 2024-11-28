8c2fde753ee73da72e9bd4a5e6c923ff64dca149
odawaNg4l4 <odawangala99@gmail.com>
2024-11-28 08:58:57 +0300

    Kilometres to miles

--- Convert Kilometres to Miles/Km's to miles.py
+++ Convert Kilometres to Miles/Km's to miles.py
@@ -1,3 +1,3 @@
 distance_kilometres = int(input('Give the distance in kilometres'))
 # distance_miles = distance_kilometres / 0.6214
-print("Distance in miles is:",    distance_kilometres * 0.6214 )
+print("Distance in miles is:", distance_kilometres * 0.6214 )

90aa344a6c437479527eec1391bd2f68b1e50a5a
odawaNg4l4 <odawangala99@gmail.com>
2024-12-09 16:27:56 +0300

    Game

--- Positive/Number guessing game/NGG.py
+++ Positive/Number guessing game/NGG.py
@@ -2,9 +2,15 @@ import random
 number = random.randint(1, 10)
 print("Welcome to the game of the year")
 
-guess = int(input("Enter your guess"))
+number_guess = int(input("Enter your guess"))
 trials = 1
-while number != guess:
-    print("Ooopsie wrong guess")
-guess = int(input("Another trial please."))
+while number != number_guess:
+    print("Oopsie wrong guess")
+    if trials == 4:
+        break
+    elif number_guess < number:
+        print("Your guess is lower")
+    else:
+        print("The guess is higher")
+
 
